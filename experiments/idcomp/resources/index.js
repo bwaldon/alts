b5cfd349d76170c1210ce965f0d46f15183033f3
brandon <bwaldon@stanford.edu>
2018-11-11 18:40:43 -0800

    initial versions of experiments for piloting

---
+++ experiments/idcomp/resources/index.js
@@ -0,0 +1,172 @@
+var order = 1;
+
+function make_slides(f) {
+  var   slides = {};
+
+  present_list = _.shuffle(control)
+
+  slides.consent = slide({
+     name : "consent",
+     start: function() {
+      exp.startT = Date.now();
+      $("#consent_2").hide();
+      exp.consent_position = 0;
+     },
+    button : function() {
+      if(exp.consent_position == 0) {
+         exp.consent_position++;
+         $("#consent_1").hide();
+         $("#consent_2").show();
+      } else {
+        exp.go(); //use exp.go() if and only if there is no "present" data.
+      }
+    }
+  });
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+slides.instructions = slide({
+    name : "instructions",
+    button : function() {
+        exp.go(); //use exp.go() if and only if there is no "present" data.    
+    }
+  });
+
+
+slides.critical = slide({
+    name : "img_select",
+
+    /* trial information for this block
+     (the variable 'stim' will change between each of these values,
+      and for each of these, present_handle will be run.) */
+
+    present : present_list,
+
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+
+      this.stim = stim; //I like to store this information in the slide so I can record it later.
+
+      $(".prompt").html('<b>What is this?</b>');
+      $(".images").html('<img src = "resources/images/' + stim.noun + '_bad.jpeg">') 
+    },
+
+    continue : function() {
+        if ($("#response").val().length == 0) {
+          $(".error").show();
+        } else {
+          this.know = true;
+          this.log_responses();
+
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+        $("#response").val('');
+        $(".error").hide();
+        }   
+      },
+
+     dunno : function() {
+        this.know = false;
+        this.log_responses();
+
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+        $("#response").val('');
+        $(".error").hide();
+      },
+
+    log_responses : function() {
+      exp.data_trials.push({
+        "response" : $("#response").val(),
+        "know" : this.know,
+        "id" : this.stim.noun,
+        "order" : order,
+      });
+    order = order + 1;
+    },
+  });
+
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.subj_data = {
+        language : $("#language").val(),
+        enjoyment : $("#enjoyment").val(),
+        asses : $('input[name="assess"]:checked').val(),
+        age : $("#age").val(),
+        gender : $("#gender").val(),
+        education : $("#education").val(),
+        comments : $("#comments").val(),
+        problems: $("#problems").val(),
+        fairprice: $("#fairprice").val()
+      };
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          // "order" : exp.order,
+          "condition" : exp.condition,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      setTimeout(function() {turk.submit(exp.data);}, 1000);
+    }
+  });
+
+  return slides;
+}
+
+/// init ///
+function init() {
+  exp.trials = [];
+  exp.catch_trials = [];
+  exp.condition = _.sample(["control","target","nottarget","symmetric"]); //can randomize between subject conditions here
+  // exp.order = _.sample(["4fillerspacing","nofillerspacing"]);
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+  //blocks of the experiment:
+  exp.structure=["i0", "consent", "instructions", "critical", 'subj_info', 'thanks'];
+
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    if (turk.previewMode) {
+      $("#mustaccept").show();
+    } else {
+      $("#start_button").click(function() {$("#mustaccept").show();});
+      exp.go();
+    }
+  });
+
+  exp.go(); //show first slide
+}
